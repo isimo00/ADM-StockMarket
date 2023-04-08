82625e71bc4588b8c3725582f24d4572c143e88e
Irene Simó Muñoz <irenemunozsimo@gmail.com>
2023-04-08 19:21:48 +0200

    imported, plotted time and autocorrelation

--- main.R
+++ main.R
@@ -1,2 +1,20 @@
+rm(list = ls())
+
 #Load data
-## More lines
+data <- read.csv2("BAJAJFINSV.csv", header=TRUE, sep=",")
+date <- as.Date(data[,1])
+for(x in 4:12){
+  data[,x] <- as.numeric(data[,x])
+}
+rm(x)
+vwap <- data[, 10]
+
+# plot VWAP (Volume Weighted Average Price)
+if (length(date) == length(vwap)){
+  plot(date, vwap, xlab = "Date", ylab = "VWAP")
+}
+
+# exploring the data
+Mlag <- as.numeric(difftime(max(date),min(date), units = "days"))
+acf(vwap, lag.max = Mlag)
+pacf(vwap, lag.max = Mlag)
