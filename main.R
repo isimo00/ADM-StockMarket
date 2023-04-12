19cf7201023d8d1d068ef0015493434605608d7f
Irene Simó Muñoz <irenemunozsimo@gmail.com>
2023-04-12 16:49:38 +0200

    ADF

--- main.R
+++ main.R
@@ -1,4 +1,7 @@
 rm(list = ls())
+library(quadprog)
+library(tseries)
+library(tsDyn)
 
 #Load data
 data <- read.csv2("BAJAJFINSV.csv", header=TRUE, sep=",")
@@ -10,11 +13,22 @@ rm(x)
 vwap <- data[, 10]
 
 # plot VWAP (Volume Weighted Average Price)
+pdf(file="figures/vwap-time.pdf", 5, 3.57)
 if (length(date) == length(vwap)){
-  plot(date, vwap, xlab = "Date", ylab = "VWAP")
+  plot(date, vwap, xlab = "Date", ylab = "VWAP", "l")
 }
+dev.off()
 
-# exploring the data
+# Augmented Dickey-Fuller Test
+adf.test(vwap, k=0)
+# Data differentiation
+vwap_diff1 <- diff(vwap, differences = 1)
+adf.test(vwap_diff1, k=0)
+
+# Autocorrelation and Partial Autocorrelation tests
 Mlag <- as.numeric(difftime(max(date),min(date), units = "days"))
 acf(vwap, lag.max = Mlag)
 pacf(vwap, lag.max = Mlag)
+
+# Granger causality test
+grangertest(vwap, y, 4) 
