0ddb503741dab6d44d3ceff5571b4c659506ca47
Irene Simó Muñoz <irenemunozsimo@gmail.com>
2023-04-13 08:21:18 +0200

    Diagnostics VAR model

--- main.R
+++ main.R
@@ -56,10 +56,29 @@ open_truncated <- open_diff1[2595:2838]
 open_var <- ts(open_truncated, start= date_truncated[1], end = date_truncated[244], frequency=0.5)
 vwap_var <- ts(vwap_truncated, start= date_truncated[1], end = date_truncated[244], frequency=0.5)
 variables_used <- cbind(vwap_var, open_var)
-lagselect <- VARselect(variables_used, lag.max = 4, type = "const", season = 4)
+colnames(variables_used) <- cbind("VWAP","Open")
+lagselect <- VARselect(variables_used, lag.max = 4, type = "const")
 lagselect$selection
-var.model_lev <- VAR(variables_used, p = 3, season = NULL, exog = NULL)
-summary(var.model_lev)
-forecast <- predict(var.model_lev, n.ahead = 12, ci = 0.95)
-fanchart(forecast, names = "vwap", main = "Fanchart for vwap", xlab = "Horizon", ylab = "RRP")
-fanchart(forecast, names = "open", main = "Fanchart for vwap", xlab = "Horizon", ylab = "RRP")
+MODEL <- VAR(variables_used, p = 3, season = NULL, exog = NULL)
+summary(MODEL)
+
+# Model diagnostics
+Serial1 <- serial.test(MODEL, lags.pt = 5, type = "PT.asymptotic")
+Serial1
+Arch1 <- arch.test(MODEL, lags.multi = 15, multivariate.only = TRUE)
+Arch1
+Norm1 <- normality.test(MODEL, multivariate.only = TRUE)
+Norm1
+Stability1 <- stability(MODEL, type = "OLS-CUSUM")
+pdf(file="figures/stability-test-VAR.pdf", 5, 3.57)
+plot(Stability1)
+dev.off()
+
+# Forecasting and simulations
+# Policy
+VWAPirf <- irf(MODEL, impulse = "VWAP", response = "VWAP", n.ahead = 20, boot = TRUE)
+plot(VWAPirf, ylab = "VWAP", main = "VWAP's shock to VWAP")
+
+forecast <- predict(MODEL, n.ahead = 12, ci = 0.95)
+fanchart(forecast, names = "VWAP", main = "Fanchart for VWAP", xlab = "Horizon", ylab = "VWAP")
+fanchart(forecast, names = "Open", main = "Fanchart for Open", xlab = "Horizon", ylab = "Open price")
