c0d1e0e783b2398ce944c3aed9a2aa5564796a8b
Irene Simó Muñoz <irenemunozsimo@gmail.com>
2023-04-16 13:29:13 +0200

    Figures and all?

--- ARIMA.R
+++ ARIMA.R
@@ -21,26 +21,32 @@ new_data <- data.frame(Date = as.Date(data[,1]),
 rm(data)
 data <- new_data[new_data$Weekday_name == "dl",]
 rm(new_data)
-VWAP <- ts(data$VWAP, start = c(2008,5,26), frequency = 51)
+VWAP <- ts(data$VWAP, start = c(2008,5,26), end= c(2019, 11, 16), frequency = 51)
 components.ts = decompose(VWAP)
+pdf(file="figures/ARIMA-vwap-components.pdf", 5, 5)
 plot(components.ts)
+dev.off()
 
-
+# Remove non-stationary
 urkpssTest(VWAP, type = c("tau"), lags = c("short"),use.lag = NULL, doplot = TRUE)
 tsstationary = diff(VWAP, differences=1)
-plot(tsstationary)
+pdf(file="figures/ARIMA-vwap-stationary.pdf", 5, 3.57)
+plot(tsstationary, ylab = "VWAP stationary")
+dev.off()
 acf(VWAP,lag.max=34) 
 timeseriesseasonallyadjusted <- VWAP- components.ts$seasonal
 tsstationary <- diff(timeseriesseasonallyadjusted, differences=1)
+
+## FITTING THE MODEL
 acf(tsstationary, lag.max=34)
 pacf(tsstationary, lag.max=34)
 
-fitARIMA <- arima(VWAP, order=c(1,1,1),seasonal = list(order = c(1,0,0), period = 12),method="ML")
+fitARIMA <- arima(VWAP, order=c(1,1,0),seasonal = list(order = c(1,0,0), period = 51),method="CSS")
 coeftest(fitARIMA) 
 confint(fitARIMA)
 
 acf(fitARIMA$residuals)
-boxresult-LjungBoxTest (fitARIMA$residuals,k=2,StartLag=1)
+boxresult-LjungBoxTest(fitARIMA$residuals,k=2,StartLag=1)
 plot(boxresult[,3],main= "Ljung-Box Q Test", ylab= "P-values", xlab= "Lag")
 qqnorm(fitARIMA$residuals)
 qqline(fitARIMA$residuals)
@@ -48,5 +54,6 @@ auto.arima(tsData, trace=TRUE)
 
 ## Forecasting
 predict(fitARIMA,n.ahead = 5)
-futurVal <- forecast(fitARIMA,h=10, level=c(99.5))
-plot(futurVal)
+#futurVal <- forecast::forecast(fitARIMA,h=10, level=c(99.5))
+futurVal <- forecast(fitARIMA, lead = 50)
+plot(futurVal, type="l")
--- VAR.R
+++ VAR.R
@@ -21,9 +21,14 @@ rm(data)
 data <- new_data[new_data$Weekday_name == "dl",]
 rm(new_data)
 
-VWAP <- ts(data$VWAP, start = c(2008,5,26), frequency = 51)
-Open <- ts(data$Open, start = c(2008,5,26), frequency = 51)
+VWAP <- ts(data$VWAP, start = c(2008,5,26), end= c(2019, 11, 16), frequency = 51)
+Open <- ts(data$Open, start = c(2008,5,26), end= c(2019, 11, 16), frequency = 51)
+pdf(file="figures/vwap-time.pdf", 5, 3.57)
 tsplot(VWAP)
+dev.off()
+pdf(file="figures/open-time.pdf", 5, 3.57)
+tsplot(Open)
+dev.off()
 
 v1 <- cbind(VWAP, Open)
 colnames(v1) <- cbind("VWAP", "Open")
@@ -31,6 +36,8 @@ lagselect <- VARselect(v1, lag.max = 15, type = "const")
 lagselect$selection
 Model1 <- VAR(v1, p = 15, type = "const", season = NULL, exog = NULL) 
 summary(Model1)
+
+## Diagnostics
 Serial1 <- serial.test(Model1, lags.pt = 5, type = "PT.asymptotic")
 Serial1
 Arch1 <- arch.test(Model1, lags.multi = 15, multivariate.only = TRUE)
@@ -40,12 +47,21 @@ Norm1
 Stability1 <- stability(Model1, type = "OLS-CUSUM")
 plot(Stability1)
 
-RRPirf <- irf(Model1, impulse = "VWAP", response = "VWAP", n.ahead = 20, boot = TRUE)
-plot(RRPirf, ylab = "VWAP", main = "VWAP's shock to VWAP")
+VWAPirf <- irf(Model1, impulse = "VWAP", response = "VWAP", n.ahead = 20, boot = TRUE)
+VWAPOpenirf <- irf(Model1, impulse = "VWAP", response = "Open", n.ahead = 20, boot = TRUE)
+Openirf <- irf(Model1, impulse = "Open", response = "Open", n.ahead = 20, boot = TRUE)
+OpenVWAPirf <- irf(Model1, impulse = "Open", response = "VWAP", n.ahead = 20, boot = TRUE)
 
-RRPirf <- irf(Model1, impulse = "Open", response = "Open", n.ahead = 20, boot = TRUE)
-plot(RRPirf, ylab = "Open", main = "Open's shock to Open")
+par(mfrow=c(2,2))
+plot(VWAPirf, ylab = "VWAP", main = "VWAP's shock to VWAP")
+plot(VWAPOpenirf, ylab = "Open", main = "Open's shock to VWAP")
+plot(Openirf, ylab = "Open", main = "Open's shock to Open")
+plot(OpenVWAPirf, ylab = "VWAP", main = "VWAP's shock to Open")
 
-forecast <- predict(Model1, n.ahead = 12, ci = 0.95)
+FEVD1 <- fevd(Model1, n.ahead = 10)
+FEVD1
+plot(FEVD1)
+
+forecast <- predict(Model1, n.ahead = 50, ci = 0.95)
 fanchart(forecast, names = "VWAP", main = "Fanchart for VWAP", xlab = "Horizon", ylab = "VWAP")
 fanchart(forecast, names = "Open", main = "Fanchart for Open", xlab = "Horizon", ylab = "Open")
